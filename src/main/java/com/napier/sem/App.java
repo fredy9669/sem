f5bc478666eddcde6f0bfefa800c1f78b28eaaa1
fredy9669 <ferdinand.havlik@gmail.com>
2022-02-06 17:44:34 +0100

    Lab 04 salaries all printed

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -145,6 +145,20 @@ public class App {
         }
     }
 
+    public static void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
 
     public static void main(String[] args)
     {
@@ -159,6 +173,9 @@ public class App {
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
+
+        //Print salaries
+        printSalaries(employees);
 
         // Disconnect from database
         a.disconnect();
